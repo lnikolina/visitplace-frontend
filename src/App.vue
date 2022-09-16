56aff07d8b34350269e6b29c17fc188fca43505a
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-16 12:59:04 +0200

    menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/beach">Beach</router-link> |
-      <router-link to="/shops">Shops</router-link> |
-      <router-link to="/history">History</router-link> |
-      <router-link to="/signup">Sign up</router-link> |
-      <router-link to="/login">Log in</router-link> 
-      
+
+    <nav class="nav justify-content-center"> 
+        <router-link class="nav-link active" to="/">Home</router-link> 
+        <router-link class="nav-link" to="/beach">Beach</router-link> 
+        <router-link class="nav-link" to="/shops">Shops</router-link> 
+        <router-link class="nav-link" to="/history">History</router-link> 
+        <router-link class="nav-link" to="/signup">Sign up</router-link> 
+        <router-link class="nav-link" to="/login">Log in</router-link> 
     </nav>
     <router-view/>
   </div>
+
 </template>
 
 
