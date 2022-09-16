aba4c0a0b6d621f1ec3791c192dda82eff3e9787
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-16 09:02:13 +0200

    pilot

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/beach">Beach</router-link> |
+      <router-link to="/fad">Food and Drink</router-link> |
+      <router-link to="/history">History</router-link>
+    
     </nav>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,21 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/beach',
+    name: 'beach',
+    component: () => import(/* webpackChunkName: "about" */ '../views/BeachView.vue')
+  },
+  {
+    path: '/fad',
+    name: 'fad',
+    component: () => import(/* webpackChunkName: "about" */ '../views/FaDView.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: () => import(/* webpackChunkName: "about" */ '../views/HistoryView.vue')
+  },
+
 ]
 
 const router = new VueRouter({
