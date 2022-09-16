7625f5eb02d8c534cc167c5d4cd14f686233236d
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-16 14:23:43 +0200

    postcard

---
+++ src/components/PostCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="card text-center">
+            <div class="card-header">
+                featured
+            </div>
+            <div class="card-body p-0">
+                <img class="card-img-top" src="../assets/slike_povijesti/bazilika.webp" alt="">
+            </div>
+            <div class="card-footer text-muted">
+                2 days ago
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostCard',
+};
+
+</script>
+
+<style lang="scss">
+.card {
+    margin-bottom: 20px;
+}
+
+</style>
