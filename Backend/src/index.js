f0d080b1d52935bb9dec887bac1eaf49b8b6494b
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-18 11:23:26 +0200

    RapidAPI client

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,11 @@
-console.log ("Hello world!")
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+   res.send("Hello world u browser") //ispis u browser ide preko res , .send ispisuje string
+   console.log("Hello world") //ispi u konzolu
+}) 
+
+app.listen(port, () => console.log ("Slušam na portu ${port}!"))
