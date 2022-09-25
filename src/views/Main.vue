0c0da22ff332da8551ddce0cb7c415181e86cf1c
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-25 16:54:12 +0200

    final

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,47 +1,43 @@
 <template>
   <div>
-    <div class="row" >
-      
+    <div class="row">
       <div v-for="imgs in imgdata" v-bind:key="imgs">
-      <br>
-        <div
-        class="d-flex justify-content-center py-2" >
-          <div class="second ">
-            <div class="text3"  >
-              <img src="~@/assets/user.png" class="imgNme"/> <span class="userNames"> {{imgs.names}}</span>
+        <br />
+        <div class="d-flex justify-content-center py-2">
+          <div class="second">
+            <div class="text3">
+              <img src="~@/assets/user.png" class="imgNme" />
+              <span class="userNames"> {{ imgs.names }}</span>
             </div>
 
-          <img class="slike" :src="imgs.imges" alt="something" />
-                <div class="thert" v-for="loc in allDataNames" v-bind:key="loc">
-  <span class="loc" v-if="imgs.imgNames === loc.imgName"><img src="~@/assets/world.png" class="imgWorld"/>{{loc.loc}}</span>
-      </div>
+            <img class="slike" :src="imgs.imges" alt="something" />
+            <div class="thert" v-for="loc in allDataNames" v-bind:key="loc">
+              <span class="loc" v-if="imgs.imgNames === loc.imgName"
+                ><img src="~@/assets/world.png" class="imgWorld" />{{
+                  loc.loc
+                }}</span
+              >
+            </div>
           </div>
-        </div >
-
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style>
-
-
-body{
-    background-image: url('https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/bg-imgs%2Ftravel.webp?alt=media&token=e1df7e3d-ea21-4819-9078-ca461e03f4fb')
-    
+body {
+  background-image: url("https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/bg-imgs%2Ftravel.webp?alt=media&token=e1df7e3d-ea21-4819-9078-ca461e03f4fb");
 }
-
-
-
 
 .slike {
   position: static;
   margin-top: 3%;
-  margin-left:  12%;
-  
+  margin-left: 12%;
+
   width: 80%;
   height: 80%;
-  border-radius:15px ;
+  border-radius: 15px;
   margin-bottom: 5%;
 }
 
@@ -53,65 +49,53 @@ body{
   margin-bottom: 5%;
   text-align: left;
   margin-top: 2%;
-  
 }
-.thert{
+.thert {
   width: 50%;
   background-color: white;
-  
+
   margin-bottom: 4%;
   margin-top: -4%;
   margin-left: 40%;
   text-align: center;
 }
 
-.text3{
+.text3 {
   top: 550px;
   text-align: center;
   border: solid white 2px;
-  
+
   background: white;
 }
-.userNames{
+.userNames {
   font-size: 18px;
-  
 }
 
-
-.imgNme{
+.imgNme {
   margin-top: -0.8%;
-  
 }
 
-.imgWorld{
+.imgWorld {
   width: 34px;
   height: 34px;
-  
 }
 
-.loc{
+.loc {
   margin-top: -0.8%;
-    font-size: 21px;
-    
+  font-size: 21px;
 }
-
-
-
 </style>
 
-
 <script>
-import { storage, ref, listAll,db,collection,getDocs } from "@/firebase";
-
+import { storage, ref, listAll, db, collection, getDocs } from "@/firebase";
 
 export default {
   name: "Home",
   data() {
     return {
       imgdata: [],
-      allDataNames:[],
-      namesOfus:[]
-
+      allDataNames: [],
+      namesOfus: [],
     };
   },
   methods: {
@@ -121,14 +105,14 @@ export default {
         .then((res) => {
           res.prefixes.forEach((folderRef) => {});
           res.items.forEach((itemRef) => {
-     
-            this.imgdata.push(
-              { imges: "https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/imgs%2F" +itemRef.name +"?alt=media",
-                names:itemRef.name.split("_", 1).join(""),
-                imgNames:itemRef.name,
-
-              }
-            );
+            this.imgdata.push({
+              imges:
+                "https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/imgs%2F" +
+                itemRef.name +
+                "?alt=media",
+              names: itemRef.name.split("_", 1).join(""),
+              imgNames: itemRef.name,
+            });
             console.log(this.imgdata);
           });
         })
@@ -137,14 +121,14 @@ export default {
         });
     },
 
-       loadNames() {
-     getDocs(collection(db, "data")).then((querySnapshot) => {
-       querySnapshot.forEach((doc) => {
-         this.allDataNames.push(doc.data());
+    loadNames() {
+      getDocs(collection(db, "data")).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.allDataNames.push(doc.data());
         });
       });
-     console.log(this.allDataNames);
-    }
+      console.log(this.allDataNames);
+    },
   },
   created: function () {
     this.loaddata();
