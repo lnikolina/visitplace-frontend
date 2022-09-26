bca42a5b9654c07ac07b5e70003a84493532cf8a
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-26 19:42:15 +0200

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,69 +1,69 @@
 <template>
-	<div>
-		<section class="image">
-			<div
-				class="image__bg"
-				style="
-					background-image: url('https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/bg-imgs%2FScreenshot%202022-09-22%20at%2019.00.54.png?alt=media&token=0cbff426-4043-4e67-91a3-76abd1534fac');
-				"
-			></div>
-			<section class="vh-100">
-				<div class="container py-5 h-100">
-					<div
-						class="row d-flex justify-content-center align-items-center h-100"
-					>
-						<div class="col-12 col-md-8 col-lg-6 col-xl-5">
-							<div class="card shadow-2-strong" style="border-radius: 1rem">
-								<div class="card-body p-5 text-center">
-									<h3 class="mb-5">Login</h3>
+  <div>
+    <section class="image">
+      <div
+        class="image__bg"
+        style="
+          background-image: url('https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/bg-imgs%2FScreenshot%202022-09-22%20at%2019.00.54.png?alt=media&token=0cbff426-4043-4e67-91a3-76abd1534fac');
+        "
+      ></div>
+      <section class="vh-100">
+        <div class="container py-5 h-100">
+          <div
+            class="row d-flex justify-content-center align-items-center h-100"
+          >
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card shadow-2-strong" style="border-radius: 1rem">
+                <div class="card-body p-5 text-center">
+                  <h3 class="mb-5">Login</h3>
 
-									<div class="form-outline mb-4">
-										<input
-											type="email"
-											v-model="email"
-											id="typeEmailX-2"
-											class="form-control form-control-lg"
-										/>
-										<label class="form-label" for="typeEmailX-2">Email</label>
-									</div>
+                  <div class="form-outline mb-4">
+                    <input
+                      type="email"
+                      v-model="email"
+                      id="typeEmailX-2"
+                      class="form-control form-control-lg"
+                    />
+                    <label class="form-label" for="typeEmailX-2">Email</label>
+                  </div>
 
-									<div class="form-outline mb-4">
-										<input
-											type="password"
-											v-model="password"
-											id="typePasswordX-1"
-											class="form-control form-control-lg"
-										/>
-										<label class="form-label" for="typePasswordX-2"
-											>Password</label
-										>
-									</div>
+                  <div class="form-outline mb-4">
+                    <input
+                      type="password"
+                      v-model="password"
+                      id="typePasswordX-1"
+                      class="form-control form-control-lg"
+                    />
+                    <label class="form-label" for="typePasswordX-2"
+                      >Password</label
+                    >
+                  </div>
 
-									<!-- Checkbox -->
+                  <!-- Checkbox -->
 
-									<button
-										class="btn btn-primary btn-lg btn-block"
-										type="button"
-										@click="login()"
-									>
-										Login
-									</button>
-									<div class="form-footer">
-										<p>
-											Don't have an account?
-											<router-link :to="{ name: 'Registracija' }"
-												>Sign Up</router-link
-											>
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		</section>
-	</div>
+                  <button
+                    class="btn btn-primary btn-lg btn-block"
+                    type="button"
+                    @click="login()"
+                  >
+                    Login
+                  </button>
+                  <div class="form-footer">
+                    <p>
+                      Don't have an account?
+                      <router-link :to="{ name: 'Registracija' }"
+                        >Sign Up</router-link
+                      >
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -72,62 +72,61 @@ import axios from "axios";
 import { mapActions, mapGetters, mapMutations } from "vuex";
 
 export default {
-	name: "Home",
+  name: "Home",
 
-	data() {
-		return {
-			email: "",
-			password: "",
-		};
-	},
-	beforeUpdate() {
-		if (this.currentUser) {
-			this.$router.replace({ name: "Main" });
-		}
-	},
-	computed: {
-		...mapGetters({ currentUser: "currentUser" }),
-	},
-	methods: {
-		...mapActions({ fetchCurrentUser: "fetchCurrentUser" }),
-		...mapMutations({ setBearerToken: "setBearerToken" }),
-		async login() {
-			try {
-				const result = await axios.post("/auth", {
-					email: this.email,
-					password: this.password,
-				});
-				localStorage.setItem("token", result.data.token);
-				await this.fetchCurrentUser();
-				this.$router.replace({ name: "Main" });
-			} catch (error) {
-				console.log(error);
-			}
-		},
-	},
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  beforeUpdate() {
+    if (this.currentUser) {
+      this.$router.replace({ name: "Main" });
+    }
+  },
+  computed: {
+    ...mapGetters({ currentUser: "currentUser" }),
+  },
+  methods: {
+    ...mapActions({ fetchCurrentUser: "fetchCurrentUser" }),
+    ...mapMutations({ setBearerToken: "setBearerToken" }),
+    async login() {
+      try {
+        const result = await axios.post("/auth", {
+          email: this.email,
+          password: this.password,
+        });
+        localStorage.setItem("token", result.data.token);
+        await this.fetchCurrentUser();
+        this.$router.replace({ name: "Main" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 
 <style>
 .image {
-	position: relative;
+  position: relative;
 
-	height: 100vh;
-	width: 100%;
+  height: 100vh;
+  width: 100%;
 }
 
 .image__bg {
-	position: absolute;
-	top: 0;
-	left: 0;
-	z-index: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
 
-	height: 100%;
-	width: 100%;
+  height: 100%;
+  width: 100%;
 
-	background-position: 50%;
-	background-repeat: no-repeat;
-	background-size: cover;
-	overflow-x: hidden;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,105 +1,105 @@
 <template>
-	<div>
-		<section class="image">
-			<div
-				class="image__bg"
-				style="
-					background-image: url('https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/bg-imgs%2Ftravel.webp?alt=media&token=e1df7e3d-ea21-4819-9078-ca461e03f4fb');
-				"
-			></div>
-			<section class="vh-100">
-				<div class="container py-5 h-100">
-					<div
-						class="row d-flex justify-content-center align-items-center h-100"
-					>
-						<div class="col-12 col-md-8 col-lg-6 col-xl-5">
-							<h2 style="color: #fff">VisitPlace</h2>
-							<div class="card shadow-2-strong" style="border-radius: 1rem">
-								<div class="card-body p-5 text-center">
-									<hr />
-									<p class="mb-5 upld">UPLOAD YOUR IMAGE</p>
-									<p class="mb-5 opl">and show it to the world</p>
+  <div>
+    <section class="image">
+      <div
+        class="image__bg"
+        style="
+          background-image: url('https://firebasestorage.googleapis.com/v0/b/visitporec-3d39f.appspot.com/o/bg-imgs%2Ftravel.webp?alt=media&token=e1df7e3d-ea21-4819-9078-ca461e03f4fb');
+        "
+      ></div>
+      <section class="vh-100">
+        <div class="container py-5 h-100">
+          <div
+            class="row d-flex justify-content-center align-items-center h-100"
+          >
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <h2 style="color: #fff">VisitPlace</h2>
+              <div class="card shadow-2-strong" style="border-radius: 1rem">
+                <div class="card-body p-5 text-center">
+                  <hr />
+                  <p class="mb-5 upld">UPLOAD YOUR IMAGE</p>
+                  <p class="mb-5 opl">and show it to the world</p>
 
-									<div class="form-outline mb-4"></div>
+                  <div class="form-outline mb-4"></div>
 
-									<input
-										type="file"
-										id="file"
-										@change="uploadFile"
-										class="uploadButton"
-										accept="image/*"
-									/>
-									<br /><br /><br />
-									<label class="form-label placeInput1" for="typeEmailX-2"
-										>Location</label
-									><br />
-									<input
-										type="Place"
-										id="place"
-										v-model="placeInput"
-										class="placeInput"
-										placeholder="Location of image"
-									/>
-									<br /><br /><br />
-									<hr />
-									<button
-										class="btn btn-primary btn-lg btn-block Disabled"
-										type="button"
-										id="btnnn"
-										@click="uploadnow()"
-									>
-										UPLOAD
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		</section>
-	</div>
+                  <input
+                    type="file"
+                    id="file"
+                    @change="uploadFile"
+                    class="uploadButton"
+                    accept="image/*"
+                  />
+                  <br /><br /><br />
+                  <label class="form-label placeInput1" for="typeEmailX-2"
+                    >Location</label
+                  ><br />
+                  <input
+                    type="Place"
+                    id="place"
+                    v-model="placeInput"
+                    class="placeInput"
+                    placeholder="Location of image"
+                  />
+                  <br /><br /><br />
+                  <hr />
+                  <button
+                    class="btn btn-primary btn-lg btn-block Disabled"
+                    type="button"
+                    id="btnnn"
+                    @click="uploadnow()"
+                  >
+                    UPLOAD
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  </div>
 </template>
 
 <style>
 .image {
-	position: relative;
+  position: relative;
 
-	height: 100vh;
-	width: 100%;
+  height: 100vh;
+  width: 100%;
 }
 
 .image__bg {
-	position: absolute;
-	top: 0;
-	left: 0;
-	z-index: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
 
-	height: 100%;
-	width: 100%;
+  height: 100%;
+  width: 100%;
 
-	background-position: 50%;
-	background-repeat: no-repeat;
-	background-size: cover;
-	overflow: hidden;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow: hidden;
 }
 
 .upld {
-	font-size: 25px;
+  font-size: 25px;
 }
 
 .opl {
-	color: #1363f8;
-	font-size: 20px;
-	margin-top: -40px;
+  color: #1363f8;
+  font-size: 20px;
+  margin-top: -40px;
 }
 
 .placeInput {
-	margin-left: 0%;
-	width: 70%;
+  margin-left: 0%;
+  width: 70%;
 }
 
 .placeInput1 {
-	margin-right: 54.2%;
+  margin-right: 54.2%;
 }
 </style>
 
@@ -109,41 +109,41 @@ import axios from "axios";
 import { mapGetters } from "vuex";
 
 export default {
-	name: "Home",
-	data() {
-		return {
-			previewFiles: null,
-			placeInput: "",
-		};
-	},
-	computed: {
-		...mapGetters({ currentUser: "currentUser" }),
-	},
-	methods: {
-		uploadFile(event) {
-			console.log(event);
-			this.previewFiles = event.target.files[0];
-		},
-		uploadnow() {
-			const splitUsr =
-				this.currentUser.firstName + " " + this.currentUser.lastName;
-			const imgeName = splitUsr + "_" + this.previewFiles.name;
+  name: "Home",
+  data() {
+    return {
+      previewFiles: null,
+      placeInput: "",
+    };
+  },
+  computed: {
+    ...mapGetters({ currentUser: "currentUser" }),
+  },
+  methods: {
+    uploadFile(event) {
+      console.log(event);
+      this.previewFiles = event.target.files[0];
+    },
+    uploadnow() {
+      const splitUsr =
+        this.currentUser.firstName + " " + this.currentUser.lastName;
+      const imgeName = splitUsr + "_" + this.previewFiles.name;
 
-			const storageRef = ref(storage, "imgs/" + imgeName);
-			if (this.placeInput.length != 0) {
-				uploadBytes(storageRef, this.previewFiles).then((snapshot) => {
-					getDownloadURL(snapshot.ref).then(async (url) => {
-						await axios.post("/posts", {
-							location: this.placeInput,
-							photoURL: url,
-						});
-						this.$router.go(this.$router.currentRoute);
-					});
-				});
-			} else {
-				alert("Please insert location");
-			}
-		},
-	},
+      const storageRef = ref(storage, "imgs/" + imgeName);
+      if (this.placeInput.length != 0) {
+        uploadBytes(storageRef, this.previewFiles).then((snapshot) => {
+          getDownloadURL(snapshot.ref).then(async (url) => {
+            await axios.post("/posts", {
+              location: this.placeInput,
+              photoURL: url,
+            });
+            this.$router.go(this.$router.currentRoute);
+          });
+        });
+      } else {
+        alert("Please insert location");
+      }
+    },
+  },
 };
 </script>
